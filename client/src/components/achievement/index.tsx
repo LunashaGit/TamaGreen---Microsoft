9a7333cd7c02b466042dbc612de225517f75891a
SashBFa <bastienfajs@gmail.com>
2022-06-24 23:50:52 +0200

    achievements page

--- client/src/components/achievement/index.tsx
+++ client/src/components/achievement/index.tsx
@@ -1,7 +1,14 @@
 const Achievement = () => {
+  const nbr: number[] = [];
+  for (let i: number = 1; i <= 16; i++) {
+    nbr.push(i);
+  }
   return (
-    <div>
-      <h2>Achievement</h2>
+    <div className="grid grid-cols-2 gap-8 p-6">
+      {nbr &&
+        nbr.map((nb) => (
+          <img src={`./images/achievements/achiev-${nb}.png`} alt={`${nb}`} />
+        ))}
     </div>
   );
 };
