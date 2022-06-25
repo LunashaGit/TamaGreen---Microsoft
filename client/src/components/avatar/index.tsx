e594745ab04ed3e0face8bd74b7a58e5c8cb6ec2
Matthew Hons <devwithmatt@gmail.com>
2022-06-25 10:44:53 +0200

    Adding the ranking component

--- client/src/components/avatar/index.tsx
+++ client/src/components/avatar/index.tsx
@@ -1,4 +1,5 @@
 const Avatar = () => {
+  
   return (
     <div className="bg-gray-500 rounded-xl row-span-4">
       <h2>Avatar</h2>
---
+++ client/src/components/ranking/Card.tsx
@@ -0,0 +1,41 @@
+import { faTrophy, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+  
+const Card = ({users, win}:any) => {
+    let colors = "";
+    let verify = false;
+    let scale = ""
+    switch(win){
+        case 1 :
+              colors = "text-amber-400"
+              scale = "scale-150"
+              verify = true;
+              break
+        case 2 :
+              colors = "text-gray-400" 
+              scale = "scale-125"
+              verify = true;
+              break
+        case 3 :
+              colors = "text-yellow-600"
+              scale = "scale-110"
+              verify = true;
+              break
+              default:
+                colors = ""
+                scale = "scale-100"
+                verify = false;
+                break
+    }
+
+    return (
+        <div className={`${scale} p-3 flex flex-col items-center justify-center`}>
+        <h2>{users.pseudo} {users.score}</h2>
+       {verify && <FontAwesomeIcon icon={faTrophy} className={`${colors} text-3xl`}/>} 
+        </div>
+       
+
+    )
+}
+export default Card;
